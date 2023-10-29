c9319c101c536be42d4d77bec4a9e389eb6a2520
Alena636 <alenahomich636@gmail.com>
2023-10-29 21:31:49 +0300

    refactor: change the path to style

--- src/widgets/SearchForm/SearchForm.tsx
+++ src/widgets/SearchForm/SearchForm.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ErrorButton from '../ErrorButton/ErrorButton';
-import '../SearchForm/searchForm.css';
+import './SearchForm.css';
 
 type SearchFormProps = {
   children?: JSX.Element;

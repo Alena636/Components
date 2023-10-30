a26b630037d90f1f9a5fb0ff47f53c42cf04a53d
Alena636 <alenahomich636@gmail.com>
2023-10-30 18:51:40 +0300

    feat: add title on the main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import SearchForm from '../../widgets/SearchForm/SearchForm';
 import CardList from '../../widgets/CardList/CardList';
+import './Main.css';
 
 type MainProps = {
   children?: JSX.Element;
@@ -24,6 +25,7 @@ export default class MainPage extends Component<MainProps, MainState> {
   render() {
     return (
       <div className="main__container">
+        <h1 className="main__title">Star Wars: People</h1>
         <SearchForm onSubmit={this.updateSearchValue} />
         <CardList data={this.state.searchValue} />
       </div>

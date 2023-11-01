01050ae147bb4f904e7fb8c4e6adf41b2b5826a9
Alena636 <alenahomich636@gmail.com>
2023-11-01 14:53:05 +0300

    refacto: change the structure of card element

--- src/widgets/Card/Card.tsx
+++ src/widgets/Card/Card.tsx
@@ -1,27 +1,25 @@
-import { Component } from 'react';
 import { CardProps } from '../../types';
 import './Card.css';
-export default class CardElement extends Component<CardProps> {
-  render() {
-    const { people } = this.props;
-    return (
-      <div className="card">
-        <h4 className="card__name">{people.name}</h4>
-        <div className="card__description">
-          <p className="card__height">
-            <span className="bold">Height: </span>
-            {people.height}
-          </p>
-          <p className="card__mass">
-            <span className="bold">Mass: </span>
-            {people.mass}
-          </p>
-          <p className="card__gender">
-            <span className="bold">Gender: </span>
-            {people.gender}
-          </p>
-        </div>
+
+export default function CardElement(props: CardProps): JSX.Element {
+  const { people } = props;
+  return (
+    <div className="card">
+      <h4 className="card__name">{people.name}</h4>
+      <div className="card__description">
+        <p className="card__height">
+          <span className="bold">Height: </span>
+          {people.height}
+        </p>
+        <p className="card__mass">
+          <span className="bold">Mass: </span>
+          {people.mass}
+        </p>
+        <p className="card__gender">
+          <span className="bold">Gender: </span>
+          {people.gender}
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }

d978f650dac67acff2fd02661a98501981edf84f
Alena636 <alenahomich636@gmail.com>
2023-11-02 13:55:55 +0300

    refactor: change the structure of main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,34 +1,20 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import SearchForm from '../../widgets/SearchForm/SearchForm';
 import CardList from '../../widgets/CardList/CardList';
 import './Main.css';
 
-type MainProps = {
-  children?: JSX.Element;
-};
+export default function MainPage(): JSX.Element {
+  const [searchValue, setSearchValue] = useState('');
 
-type MainState = {
-  searchValue: string;
-};
-export default class MainPage extends Component<MainProps, MainState> {
-  constructor(props: MainProps | Readonly<MainProps>) {
-    super(props);
-    this.state = {
-      searchValue: '',
-    };
-  }
-
-  updateSearchValue = (value: string) => {
-    this.setState({ searchValue: value });
+  const updateSearchValue = (value: string): void => {
+    setSearchValue(value);
   };
 
-  render() {
-    return (
-      <div className="main__container">
-        <h1 className="main__title">Star Wars: People</h1>
-        <SearchForm onSubmit={this.updateSearchValue} />
-        <CardList data={this.state.searchValue} />
-      </div>
-    );
-  }
+  return (
+    <div className="main__container">
+      <h1 className="main__title">Star Wars: People</h1>
+      <SearchForm onSubmit={updateSearchValue} />
+      <CardList data={searchValue} />
+    </div>
+  );
 }

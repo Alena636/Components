49442e56ae00065c3b582ddba3f177b40eba12aa
Alena636 <alenahomich636@gmail.com>
2023-12-03 00:25:01 +0300

    refactor: change the structure uncontrolled form

--- src/pages/UncontrolledForm/UncontrolledForm.tsx
+++ src/pages/UncontrolledForm/UncontrolledForm.tsx
@@ -1,44 +1,48 @@
 const UncontrolledForm: React.FC = () => {
   return (
     <form className="form">
-      <label>
+      <label className="form__label" htmlFor="name">
         Name
-        <input type="text" />
+        <input type="text" id="name" />
       </label>
-      <label>
+      <label className="form__label" htmlFor="age">
         Age
-        <input type="number" />
+        <input type="number" id="age" />
       </label>
-      <label>
+      <label className="form__label" htmlFor="email">
         Email
-        <input type="email" />
+        <input type="email" id="email" />
       </label>
-      <label>
+      <label className="form__label" htmlFor="password">
         Password
-        <input type="password" />
+        <input type="password" id="password" />
       </label>
-      <label>
+      <label className="form__label" htmlFor="repeatPassword">
         Repeat your password
-        <input type="password" />
+        <input type="password" id="repeatPassword" />
       </label>
-      <label>
-        Gender
-        <input type="radio" name="gender" value="male" />
-        Male
-        <input type="radio" name="gender" value="female" />
-        Female
+      <div className="form__gender">
+        <p className="gender__title">Gender</p>
+        <label className="form__label" htmlFor="male">
+          Male
+          <input type="radio" id="male" name="gender" />
+        </label>
+        <label className="form__label" htmlFor="female">
+          Female
+          <input type="radio" id="female" name="gender" />
+        </label>
+      </div>
+      <label className="form__label" htmlFor="terms">
+        <input type="checkbox" name="terms" value="agree" id="terms" />I agree
+        to the terms and conditions
       </label>
-      <label>
-        <input type="checkbox" name="terms" value="agree" />I agree to the terms
-        and conditions
-      </label>
-      <label>
+      <label className="form__label" htmlFor="img">
         Upload Picture
-        <input type="file" accept=".png, .jpeg" />
+        <input type="file" accept=".png, .jpeg" id="img" />
       </label>
-      <label>
+      <label className="form__label" htmlFor="country">
         Country
-        <input type="text" />
+        <input type="text" id="country" />
       </label>
       <button type="submit">Submit</button>
     </form>
